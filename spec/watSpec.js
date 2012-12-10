f24b2882302d2cabf2001cbc061ed59fd2716346
Jed Northridge <northridge@gmail.com>
2012-12-09 21:21:55 -0500

    [Wat] {} + [] == ???, {} + {} == ???

--- spec/watSpec.js
+++ spec/watSpec.js
@@ -12,4 +12,25 @@ describe('wat', function() {
     expect(typeof arrayPlusObject).toBe('string');
   });
 
+  it ('{} + [] is 0', function() {
+    var objectPlusArray = {} + [];
+    // Hah! Don't understand why Wat isn't working.
+    // Here's what I expect it to be:
+    // expect(objectPlusArray).toBe(0);
+    // expect(typeof objectPlusArray).toBe('number');
+    // But here's what I'm seeing:
+    expect(objectPlusArray).toBe('[object Object]');
+    expect(typeof objectPlusArray).toBe('string');
+  });
+
+  it ('{} + {}', function() {
+    var objectPlusObject = {} + {};
+    // Hah! Don't understand why Wat isn't working.
+    // Here's what I expect it to be:
+    // expect(objectPlusObject).toBe(NaN);
+    //
+    // But here's what I'm seeing:
+    expect(objectPlusObject).toBe('[object Object][object Object]');
+  });
+
 });
